5080eb19d865dcc1d2795eb045c735b62f315e9d
Joe McDonald <joemcd0224@gmail.com>
2025-07-02 12:15:53 -0700

    code fix for individual tournament result

--- individual_tournament_results.py
+++ individual_tournament_results.py
@@ -9,6 +9,56 @@ headers = {
     "referer": "https://www.pgatour.com"
 }
 
+all_ids_by_year = {}
+
+for year in range(2012, 2026):
+    query = {
+        "operationName": "Schedule",
+        "variables": {
+            "tourCode": "R",
+            "year": f"{year}"
+        },
+        "query": """
+        query Schedule($tourCode: String!, $year: String!) {
+            schedule(tourCode: $tourCode, year: $year) {
+            completed {
+            month
+            tournaments {
+                tournamentName
+                id
+                champion
+                championId
+                championEarnings
+                startDate
+            }
+            }
+        }
+        }
+        """
+    }
+
+    response = requests.post(url, headers=headers, json=query)
+    data = response.json()
+
+    tournaments = []
+
+
+    try:
+        for month in data["data"]["schedule"]["completed"]:
+            for t in month["tournaments"]:
+                # Store tournament IDs for this year
+                all_ids_by_year[year] = [{"id": t["ID"], "name": t["Tournament"]} for t in tournaments]
+                tournaments.append({
+                    "Tournament": t.get("tournamentName"),
+                    "ID": t.get("id"),
+                    "Champion": t.get("champion"),
+                    "Champion ID": t.get("championId"),
+                    "Earnings": t.get("championEarnings"),
+                    "Start Date": pd.to_datetime(t.get("startDate"), unit="ms").date() if t.get("startDate") else None
+                })
+    except Exception as e:
+        print(f"Error parsing data: {e}")
+
 for year in range(2012, 2026):
     for tournament in all_ids_by_year[year]:
         tournament_id = tournament["id"]
